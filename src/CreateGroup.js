0137007f2c4877f9fe8923aaf52d631337ed97b4
Isaac Dodrill <Isaac Dodrill>
2017-05-17 10:04:13 -0700

    changed name to CreateGroup

--- src/CreateGroup.js
+++ src/CreateGroup.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 /*This file displays the create/join room page*/
 
-class CreateRoom extends Component {
+class CreateGroup extends Component {
 
   componentWillReceiveProps = (newProps) => {
     //Check for prop changes, and set state from here if something new comes up, since render does not re render component.
@@ -11,10 +11,10 @@ class CreateRoom extends Component {
   render() {
     return (
       <section className="container">
-        <p>This is the create room page. Testing passed in prop via routing: {this.props.auth ? 'Auth is true' : 'Auth is false'}</p>
+        <p>This is the create group page. Testing passed in prop via routing: {this.props.auth ? 'Auth is true' : 'Auth is false'}</p>
       </section>
     );
   }
 }
 
-export default CreateRoom;
+export default CreateGroup;
