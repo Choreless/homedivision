76d67e605ca09515aada955e4df488f09ccb7a32
jimmybnguyen <jimmyn9@uw.edu>
2017-05-16 18:05:00 -0700

    add react-grid-layout to package, weekly cleanup

--- src/Weekly.js
+++ src/Weekly.js
@@ -8,6 +8,7 @@ import './weekly.css';
 /*This file handles display of the weekly calendar*/
 
 class Weekly extends Component {
+    
     static defaultProps = { 
         className: "layout",
         rowHeight: 30,
@@ -15,6 +16,7 @@ class Weekly extends Component {
         cols: {lg: 12, md: 10, sm: 6, xs: 4, xxs: 2},
         initialLayout: generateLayout()
     };    
+
     constructor(props) {
         super(props);
         this.state = {
@@ -44,39 +46,20 @@ class Weekly extends Component {
       };
 
     generateDOM() {
-    return _.map(this.state.layouts.lg, function (l, i) {
-      return (
-        <div key={i} className={l.static ? 'static' : ''}>
-          {l.static ?
-            <span className="text" title="This item is static and cannot be removed or resized.">Static - {i}</span>
-            : <span className="text">{i}</span>
-          }
-        </div>);
-    });
-  }
+        return _.map(this.state.layouts.lg, function (l, i) {
+            return (
+                <div key={i} className={l.static ? 'static' : ''}>
+                {l.static ?
+                    <span className="text" title="This item is static and cannot be removed or resized.">Static - {i}</span>
+                    : <span className="text">{i}</span>
+                } 
+                </div>);
+            });
+        }
+    
+                     
   render() {
-      var layoutNonState = [
-          {i: 'a', x: 0, y: 0, w: 3, h: 2, isResizable: false},
-          {i: 'b', x: 1, y: 0, w: 3, h: 2, isResizable: false},
-          {i: 'c', x: 4, y: 0, w: 3, h: 2, isResizable: false}
-      ];
-
     return (
-    /*
-      <ReactGridLayout className="layout" layout={layoutNonState} cols={12} rowHeight={30} width={900}>
-        <div key={'a'}>a</div>
-        <div key={'b'}>b</div>
-        <div key={'c'}>c</div>
-      </ReactGridLayout>*/
-        /*
-    <ResponsiveReactGridLayout className="layout" layouts={this.state.layouts}
-      breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-      cols={{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}}>
-      <div key={'a'}>a</div>
-      <div key={'b'}>b</div>
-      <div key={'c'}>c</div>
-    </ResponsiveReactGridLayout>*/
-        
       <div>
         <ResponsiveReactGridLayout
           {...this.props}
@@ -91,29 +74,20 @@ class Weekly extends Component {
     );
   }
 }
-/*
-function generateLayout() {
-    var layout = [
-      {i: 'a', x: 0, y: 0, w: 3, h: 2, minW: 3, maxW: 3, minH: 2, maxH: 2},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 3, maxW: 3, minH: 2, maxH: 2},
-      {i: 'c', x: 4, y: 0, w: 3, h: 2, minW: 3, maxW: 3, minH: 2, maxH: 2}
-    ];  
-    return layout;
-}*/
 
 function generateLayout() {
-  return _.map(_.range(0, 20), function (item, i) {
-    var y = Math.ceil(Math.random() * 4) + 1;
-      console.log(i);
-    return {
-      x: _.random(0, 5) * 2 % 12,
-      y: Math.floor(i / 6) * y,
-      w: 1.5,
-      h: 2,
-      i: i.toString(),
-      static: Math.random() < 0.05
-    };
-  });
+    return _.map(_.range(0, 20), function (item, i) {
+        var y = Math.ceil(Math.random() * 4) + 1;
+        return {
+            x: _.random(0, 5) * 2 % 12,
+            y: Math.floor(i / 6) * y,
+            w: 1.5,
+            h: 2,
+            i: i.toString(),
+            isResizable: false,
+            static: Math.random() < 0.05
+        };
+    });
 }
 
 export default Weekly;
