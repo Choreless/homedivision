73a777fad6dcd9534ceb9e2e344f431a5523ccbc
jimmybnguyen <jimmyn9@uw.edu>
2017-05-22 10:36:23 -0700

    add 9 col

--- src/Weekly.js
+++ src/Weekly.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import firebase from 'firebase';
+import firebase from 'firebase';
 import PropTypes from 'prop-types';
 import {Responsive, WidthProvider} from 'react-grid-layout';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
@@ -7,23 +7,23 @@ import WeeklyDays from './WeeklyDays.js';
 import './weekly.css';
 var _ = require('lodash');
 
-//var generalRef = firebase.database().ref("groups/dw23498xz/chores");
+//var choresRef = firebase.database().ref("groups/dw23498xz/chores");
 
 /*This file handles display of the weekly calendar*/
 
 class Weekly extends Component {
-    
+
     static propTypes = {
         onLayoutChange: PropTypes.func.isRequired
     };    
     
-    // This code is for a responsive grid layout; however, I set all the columns to be 8 (days + col for card deck)
+    // This code is for a responsive grid layout; however, I set all the columns to be 9 (days + col for card deck + col for members)
     // since we will be switching to a different view on a md or lower breakpoint 
     static defaultProps = { 
         className: "layout",
         rowHeight: 30,
         onLayoutChange: function() {},
-        cols: {lg: 8, md: 8, sm: 8, xs: 8, xxs: 8},
+        cols: {lg: 9, md: 9, sm: 9, xs: 9, xxs: 9},
         initialLayout: generateLayout()
     };    
 
@@ -33,12 +33,26 @@ class Weekly extends Component {
             currentBreakpoint: 'lg',
             mounted: false,
             layouts: {lg: this.props.initialLayout},
+            groupID: 'dw23498xz',
+            chores: []
         };
     }
 
     componentDidMount() {
-        this.setState({mounted: true});
-    }
+        this.setState({
+            mounted: true
+        });
+
+        firebase.database().ref('groups/' + this.state.groupID).on('value', (snapshot) => {
+            const currentChores = snapshot.val();
+            if (currentChores != null) {
+                this.setState({
+                    chores: currentChores
+                });
+            }
+            console.log(this.state.chores);
+        })
+    };
 
     onBreakpointChange = (breakpoint) => {
         this.setState({
@@ -88,7 +102,8 @@ class Weekly extends Component {
     }
 }
 
-// Sets the properties for each chore card
+// Generate the layout of the chore cards
+// Returns an array of objects
 // x is the x position on the grid, defaults to 8, the chore deck column 
 // y is the y position on the grid
 // w and h are width and height
@@ -97,7 +112,7 @@ function generateLayout() {
     return _.map(_.range(0, 10), function (item, i) {
         var y = Math.ceil(Math.random() * 4) + 1;
         return {
-            x: 8,
+            x: 9,
             y: Math.floor(i / 6) * y,
             w: 1,
             h: 2,
