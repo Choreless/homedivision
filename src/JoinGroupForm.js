4b422e6dfcf5a8ef5416752d23436a4fb6f3e423
Isaac Dodrill <Isaac Dodrill>
2017-05-24 11:49:51 -0700

    created components for creategroup and joingroup

---
+++ src/JoinGroupForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {TextField, RaisedButton, CircularProgress} from 'material-ui';
+
+class JoinGroupForm extends Component{
+    
+    state = {
+        passcode: undefined,
+        errorText: '',
+        disabled: true,
+        icon: undefined
+    };
+
+    //handle join group button
+    joinGroup = event => {
+
+    }
+    
+    handleChange = (event) => {
+        var field = event.target.name;
+        var value = event.target.value;
+        var changes = {}; //object to hold changes
+        changes[field] = value; //change this field
+        this.setState(changes); //update state
+        this.setState({errorText: ''});
+        if(this.state.passcode) this.setState({disabled: false});
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h4>Join a Group</h4>
+                <div style={{color: '#E53935'}}>{this.state.errorText}</div>
+                <form role="form" onSubmit={this.joinGroup}>
+                    <div className="input-field">
+                        <TextField style={{color: '#039BE5'}} floatingLabelText="Passcode to Join" fullWidth={true} type="passcode" name="passcode" onChange={(e) => {this.handleChange(e)}}/>
+                    </div>
+                    
+                    <RaisedButton type="submit" label={!this.state.icon && 'Join Group'} icon={this.state.icon} primary={true} disabled={this.state.disabled} labelStyle={{color: '#fff'}} onTouchTap={this.joinGroup}/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default JoinGroupForm;
