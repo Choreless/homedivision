a49dc0d16f61dd362aaed19046e99ac81687fda6
Albert Corsten <corstena@uw.edu>
2017-05-29 15:37:18 -0700

    Updated base cod e for user settings

--- src/UserSettings.js
+++ src/UserSettings.js
@@ -6,6 +6,7 @@ import { TextField, RaisedButton, List, ListItem, Subheader, CircularProgress, C
 import EditIcon from 'material-ui/svg-icons/editor/mode-edit';
 
 class UserSettings extends Component {
+
   state = {
     isAuth: undefined,
     open: false,
@@ -13,92 +14,158 @@ class UserSettings extends Component {
     nonAuthText: ''
   }
 
-  updateNickname() {
-    return (
-      <div>
-        <h2>Update Nickname</h2>
-        <form role="form" /*onSubmit={this.signIn}*/>
-          <div className="updateNickname">
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-              <TextField style={{color: '#039BE5'}} floatingLabelText="New Nickname" fullWidth={true} type="nickname" name="nickname" />
-            </MuiThemeProvider>
-          </div>
-        </form>
-      </div>
-    );
+  validate = (value, validations) => {
+    var errors = {isValid: true, style:''};
+
+    if(value !== undefined){ //check validations
+      //handle required
+      if(validations.required && value === ''){
+        errors.required = true;
+        errors.isValid = false;
+      }
+
+      //handle minLength
+      if(validations.minLength && value.length < validations.minLength){
+        errors.minLength = validations.minLength;
+        errors.isValid = false;
+      }
+      if(validations.passwordInput && validations.passwordInput !== value){
+        errors.passwordInput = true;
+        errors.isValid = false;
+      }
+
+      let valid;
+      //handle email type ??
+      if(validations.email){
+        //pattern comparison from w3c
+        //https://www.w3.org/TR/html-markup/input.email.html#input.email.attrs.value.single
+        valid = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)
+        if(!valid){
+          errors.email = true;
+          errors.isValid = false;
+        }
+      }
+
+      if(validations.user) {
+        valid = !/[^a-zA-Z0-9]/.test(value)
+        if(!valid) {
+          errors.isValid = false;
+          errors.specialcharacters = true;
+        }
+      }
+
+      //handle password type
+      if(validations.password){
+        valid = /^(?=.*\d+)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%]{6,15}$/.test(value)
+        if(!valid){
+          errors.password = true;
+          errors.isValid = false;
+        }
+      }
+    }
+
+    //display details
+    if(!errors.isValid){ //if found errors
+      errors.style = 'has-error';
+    }
+    else if(value !== undefined){ //valid and has input
+      //errors.style = 'has-success' //show success coloring
+    }
+    else { //valid and no input
+      errors.isValid = false; //make false anyway
+    }
+    return errors; //return data object
   }
 
-  updatePassword() {
-    return (
-      <div>
-        <h2>Update Password</h2>
-        <form role="form" /*onSubmit={this.signIn}*/>
-          <div className="confirmPassword">
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-              <TextField style={{color: '#039BE5'}} floatingLabelText="Old Password" fullWidth={true} type="old_password" name="old_password" />
-            </MuiThemeProvider>
-          </div>
-        </form>
-        <form role="form" /*onSubmit={this.signIn}*/>
-          <div className="newPassword">
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-              <TextField style={{color: '#039BE5'}} floatingLabelText="New Password" fullWidth={true} type="new_password" name="new_password" />
-            </MuiThemeProvider>
-          </div>
-        </form>
-        <form role="form" /*onSubmit={this.signIn}*/>
-          <div className="confirmNewPassword">
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-              <TextField style={{color: '#039BE5'}} floatingLabelText="Confirm New Password" fullWidth={true} type="confirm_new_password" name="confirm_new_password" />
-            </MuiThemeProvider>
-          </div>
-        </form>
-      </div>
-    );
+   handleUserValidate = (event) => {
+    this.handleChange(event);
+    let errors = this.validate(event.target.value, {required:true, minLength:3, user:true});
+    this.setState({uservalidate: errors.isValid});
   }
 
-  updateEmail() {
-    return (
-      <div>
-        <h2>Update Email</h2>
-        <form role="form" /*onSubmit={this.signIn}*/>
-          <div className="updateNickname">
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-              <TextField style={{color: '#039BE5'}} floatingLabelText="New Email" fullWidth={true} type="email" name="email" />
-            </MuiThemeProvider>
-          </div>
-        </form>
-      </div>
-    );
+    handlePasswordValidate = (event) => {
+    this.handleChange(event);
+    let errors = this.validate(event.target.value, {required:true, minLength:6, password:true});
+    this.setState({passwordvalidate: errors.isValid})
   }
 
-  updateColor() {
-    return (
-      <div>
-        <h2>Update Personal Color</h2>    
-        
-      </div>
-    );
+  handleMatchValidate = (event) => {
+    this.handleChange(event);
+    let errors = this.validate(event.target.value, {required:true, passwordInput:this.state.password})
+    this.setState({matchvalidate: errors.isValid})
   }
 
-  UserSettingsForm() {
-    return (
-      <div>
-        {this.updateNickname()}
-        {this.updatePassword()}
-        {this.updateEmail()}
-        {this.updateColor()}
-      </div>
-          
-    );
+  updateNickname = (event) => {
+    
   }
+
+  updatePassword = (event) => {
+
+  }
+
+  updateEmail = (event) => {
+
+  }
+
+  updateColor = (event) => {
+
+  }
+
 
   render() {
     //Variables go here
     return (
       <section className="container">
-        <h1>User Settings</h1>
-        {this.UserSettingsForm()}
+        <h2>User Settings</h2>
+        <div style={{color: '#E53935'}}>{this.state.errorText}</div>
+        <div>
+          <h4>Update Nickname</h4>
+          <form role="form" /*onSubmit={this.signIn}*/>
+            <div className="updateNickname">
+              <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <TextField id="signin-user" style={{color: '#039BE5'}} fullWidth={true} floatingLabelText="New Nickname" name="user" type="text" onChange={this.handleUserValidate} errorText={!this.state.uservalidate && this.state.user ? 'Must be at least 3 characters in length and not contain special characters or spaces':''} />
+              </MuiThemeProvider>
+              
+            </div>
+          </form>
+        </div>
+        <div>
+          <h4>Update Password</h4>
+          <form role="form" /*onSubmit={this.signIn}*/>
+            <div className="confirmPassword">
+              <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <TextField style={{color: '#039BE5'}} floatingLabelText="Old Password" fullWidth={true} type="old_password" name="old_password" />
+              </MuiThemeProvider>
+            </div>
+          </form>
+          <form role="form" /*onSubmit={this.signIn}*/>
+            <div className="newPassword">
+              <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <TextField style={{color: '#039BE5'}} floatingLabelText="New Password" fullWidth={true} type="new_password" name="new_password" />
+              </MuiThemeProvider>
+            </div>
+          </form>
+          <form role="form" /*onSubmit={this.signIn}*/>
+            <div className="form">
+              <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <TextField id="signin-user" style={{color: '#039BE5'}} fullWidth={true} floatingLabelText="Confirm New Password" name="match" type="password" onChange={this.handleMatchValidate} errorText={!this.state.matchvalidate && this.state.match ? 'Passwords do not match':''} />
+              </MuiThemeProvider>
+            </div>
+          </form>
+        </div>
+        <div>
+          <h4>Update Email</h4>
+          <form role="form" /*onSubmit={this.signIn}*/>
+            <div className="updateNickname">
+              <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <TextField style={{color: '#039BE5'}} floatingLabelText="New Email" fullWidth={true} type="email" name="email" />
+              </MuiThemeProvider>
+            </div>
+          </form>
+        </div>
+        <div>
+          <h4>Update Personal Color</h4>
+        </div>
       </section>
     );
   } 
