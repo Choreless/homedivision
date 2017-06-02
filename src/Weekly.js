b9c185b68ef8c9b045c33b334ab65570602a2896
FXStrato <jz.liberation@gmail.com>
2017-06-01 17:19:29 -0700

    Need to rework mobile weekly view, slimmed it down

--- src/Weekly.js
+++ src/Weekly.js
@@ -192,6 +192,11 @@ class Weekly extends Component {
 
 
     render() {
+      let chores = _.map(this.state.items, (elem,index) => {
+        return (
+          <li key={'Chore-'+index}>{elem.chore}</li>
+        )
+      })
         return (
           <div>
             {this.state.isMobile ?
@@ -203,53 +208,10 @@ class Weekly extends Component {
                 </Row>
                 <Row>
                   <Col s={12}>
-                    <h3 style={{fontSize: '1.4rem'}}>Members</h3>
-                    <Collapsible popout>
-                      <CollapsibleItem header='Group Member 1'>
-                        This is the monthly calendar view, with room ID: {this.props.match.params.roomID}
-                      </CollapsibleItem>
-                      <CollapsibleItem header='Group Member 2'>
-                        <Row>
-                          <Col s={10}>
-                            <MuiThemeProvider muiTheme={getMuiTheme()}>
-                              <Table
-                                selectable={false}>
-                                <TableHeader
-                                  displaySelectAll={false}
-                                  adjustForCheckbox={false}>
-                                  <TableRow>
-                                    <TableHeaderColumn>Chore</TableHeaderColumn>
-                                    <TableHeaderColumn>Time</TableHeaderColumn>
-                                    <TableHeaderColumn>Edit</TableHeaderColumn>
-                                  </TableRow>
-                                </TableHeader>
-                                <TableBody
-                                  displayRowCheckbox={false}>
-                                  <TableRow>
-                                    <TableRowColumn>Clean Dishes</TableRowColumn>
-                                    <TableRowColumn>Mon</TableRowColumn>
-                                    <TableRowColumn><IconButton><EditIcon/></IconButton></TableRowColumn>
-                                  </TableRow>
-                                  <TableRow>
-                                    <TableRowColumn>Do Laundry</TableRowColumn>
-                                    <TableRowColumn>Sun</TableRowColumn>
-                                    <TableRowColumn><IconButton><EditIcon/></IconButton></TableRowColumn>
-                                  </TableRow>
-                                </TableBody>
-                              </Table>
-                            </MuiThemeProvider>
-                          </Col>
-                          <Col s={2}>
-                            <MuiThemeProvider muiTheme={getMuiTheme()}>
-                              <IconButton iconStyle={{height: 36, width: 36}} tooltip="Add Chore Card" onTouchTap={this.handleOpen}><ActionAddNote/></IconButton>
-                            </MuiThemeProvider>
-                          </Col>
-                        </Row>
-                      </CollapsibleItem>
-                      <CollapsibleItem header='Group Member 3'>
-                        Lorem ipsum dolor sit amet.
-                      </CollapsibleItem>
-                    </Collapsible>
+                    List of chores <br/>
+                    <ul>
+                      {chores}
+                    </ul>
                   </Col>
                 </Row>
               </section>
